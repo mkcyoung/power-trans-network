1fe511cb30052a7771079d4442b64926a1317133
mkcyoung <m.c.young0@gmail.com>
2019-10-19 19:33:08 -0600

    Got a network up for power system

--- js/pownet.js
+++ js/pownet.js
@@ -3,12 +3,90 @@ class PowNet {
 
     // Creates a Power Network object
     constructor(data){
+        //Assigning data variable
         this.data = data;
+
+        //Margins - the bostock way
+        this.margin = {top: 20, right: 20, bottom: 20, left: 20};
+        this.width = 700 - this.margin.left - this.margin.right;
+        this.height = 700 - this.margin.top-this.margin.bottom; 
     }
 
     /** Builds network based on data passed into object */
     createNet(){
         console.log("Power Network Onject: ",this.data)
+        
+        //Select view1 and append an svg to it
+        let powSVG = d3.select(".view1").append("svg")
+            .attr("height",this.height+this.margin.top+this.margin.bottom)
+            .attr("width",this.width+this.margin.left+this.margin.right);
+
+        let netGroup = powSVG.append("g")
+            .attr("transform","translate("+this.margin.left+","+this.margin.top+")");
+        
+        //Going to do this with a force-directed graph first
+        var simulation = d3.forceSimulation()
+            .force("link", d3.forceLink().id(function(d) { return d.id; }))
+            .force("charge", d3.forceManyBody().strength(-20))
+            .force("center", d3.forceCenter(this.width / 2, this.height / 2));
+
+        // First we create the links in their own group that comes before the node 
+        //  group (so the circles will always be on top of the lines)
+        let linkLayer = netGroup.append("g")
+            .attr("class", "links");
+         // Now let's create the lines
+        let links = linkLayer.selectAll("line")
+            .data(this.data.links)
+            .enter().append("line");
+
+        // Now we create the node group, and the nodes inside it
+        let nodeLayer = netGroup.append("g")
+            .attr("class", "nodes");
+        let nodes = nodeLayer
+            .selectAll("circle")
+            .data(this.data.nodes)
+            .enter().append("circle")
+            .attr("r", 5);
+        
+        //Add tooltip to nodes
+        nodes.append("title")
+                .text( d => d.id );
+        
+
+        // Feeding data to simulation
+        simulation.nodes(this.data.nodes);
+        simulation.force("link")
+                .links(this.data.links);
+
+        
+        // Finally, let's tell the simulation how to update the graphics
+        simulation.on("tick", function () {
+            // Every "tick" of the simulation will create / update each node's 
+            //  coordinates; we need to use those coordinates to move the lines
+            //  and circles into place
+            links
+                .attr("x1", function (d) {
+                    return d.source.x;
+                })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            nodes
+                .attr("cx", function (d) {
+                    return d.x;
+                })
+                .attr("cy", function (d) {
+                    return d.y;
+                });
+        });
+
         
 
 
--- js/script.js
+++ js/script.js
@@ -12,12 +12,6 @@
  * 3) Active Power flow
  */
  
-//Initializaing power network object
-this.powNet = {
-    "nodes":[],
-    "links":[]
-};
-let that = this;
 
 Promise.all([
     d3.csv("data/PowerSystem_csvs/activeLoad.csv"),
@@ -28,9 +22,15 @@ Promise.all([
     d3.csv("data/PowerSystem_csvs/activePowerFlow.csv")
 ]).then(function(files){
 
+    //Initializaing power network object
+    let powNet = {
+        "nodes":[],
+        "links":[]
+    };
+
     //Adding active load to power net object w/ id
     files[0].forEach( (d, i) => {
-        that.powNet.nodes.push({
+        powNet.nodes.push({
             "id": d[""],
             "aLoad": Object.entries(d).slice(1),
             "volt": null,
@@ -40,15 +40,15 @@ Promise.all([
 
     //Adding voltage to power net object nodes
     files[1].forEach( (d, i) => {
-        if (that.powNet.nodes[i].id == d[""]){
-            that.powNet.nodes[i].volt = Object.entries(d).slice(1)
+        if (powNet.nodes[i].id == d[""]){
+            powNet.nodes[i].volt = Object.entries(d).slice(1)
         }
     });
 
     //Adding charging station power to correct nodes
     stations = ["n2","n13","n9","n33","n25","n31","n8"];
     files[2].forEach( (d, i) => {
-        that.powNet.nodes.forEach( (e,j) => {
+        powNet.nodes.forEach( (e,j) => {
             if(e.id == stations[i]){
                 e["chSP"] = Object.entries(d).slice(1);
             } 
@@ -57,9 +57,9 @@ Promise.all([
 
     //Creating link properties and adding in current
     files[3].forEach( (d, i) => {
-        that.powNet.links.push({
-            "From": d.From,
-            "To": d.To,
+        powNet.links.push({
+            "source": d.From,
+            "target": d.To,
             "current": Object.entries(d).slice(2),
             "mLC": null,
             "aPF": null
@@ -68,22 +68,22 @@ Promise.all([
 
     //Add maximum line currents to links
     files[4].forEach( (d, i) => {
-        if((d.From == that.powNet.links[i]['From']) && (d.To == that.powNet.links[i]['To'])){
-            that.powNet.links[i].mLC = +d.Imax;
+        if((d.From == powNet.links[i]['source']) && (d.To == powNet.links[i]['target'])){
+            powNet.links[i].mLC = +d.Imax;
         }
     });
 
     //Adding in active power flow
     files[5].forEach( (d, i) => {
-        if ((d.From == that.powNet.links[i]['From']) && (d.To == that.powNet.links[i]['To'])){
-            that.powNet.links[i].aPF = Object.entries(d).slice(2)
+        if ((d.From == powNet.links[i]['source']) && (d.To == powNet.links[i]['target'])){
+            powNet.links[i].aPF = Object.entries(d).slice(2)
         }
     });
-
-    //console.log("Power Net: ",that.powNet);
+    
+    console.log("Power Net: ",powNet);
 
     /** Pass data into PowNet class */
-    let powNetwork = new PowNet(that.powNet);
+    let powNetwork = new PowNet(powNet);
     powNetwork.createNet();
 
 });
