c6ae8af94b110f99d0456afd51e9c8eea06b557c
mkcyoung <m.c.young0@gmail.com>
2019-10-24 21:12:00 -0600

    starting on table

--- js/table.js
+++ js/table.js
@@ -1,10 +1,38 @@
 /** Class implementing the table */
 class Table{
 
-    constructor(data){
-        this.data = data;
+    constructor(BEBdata,station_Data){
+        this.BEB = BEBdata;
+        this.station = station_Data;
 
+        //Margins for table cells- the bostock way
+        this.margin = {top: 0, right: 10, bottom: 0, left: 10};
+        this.width = 150 - this.margin.left - this.margin.right;
+        this.height = 30 - this.margin.top-this.margin.bottom;
 
+        // I'm gonna make this an array of objects with keys and sorted values
+        this.tableHeaders = [
+            {
+                'key':"BEB",
+                'sorted': false
+            },
+            {
+                'key':"Location",
+                'sorted': false
+            },
+            {
+                'key': "Energy",
+                'sorted': false
+            },
+            {
+                'key': "Power",
+                'sorted': false
+            },
+            {
+                'key':'Speed',
+                'sorted': false
+            }
+        ];
 
 
     }
